aa64b5a3c5167d475b981f0350e32fbdd056a69e
Artefakt-ff <fasqwerty123@mail.ru>
2018-11-17 03:39:04 +0300

    Сделал первую секцию без серверной части и смайлов, я молодец, пришлось отказаться от "облачков" из-за сложности в css верстке, сделал а-ля телеграмм. Написал алгоритм ввода сообщений(vuejs), c помощью jquery перехватываю нажатие Enter.

--- js/messages.js
+++ js/messages.js
@@ -1,7 +1,17 @@
+$(function () {  // catch enter press
+    $('body').keypress(function (event) {
+        var key = event.keycode || event.which;
+        if (key === 13) {
+            $('#main_send').click();
+        }
+
+    });
+});
+
 Vue.component('message', {
-    props: ['text', 'sender'],
+    props: ['text', 'sender', 'author'],
     template: '<div class="message">' +
-        '<div class="from" v-bind:class="sender"> {{text}} </div>' +
+        '<div class="from" v-bind:class="sender"><span v-if="author" class="author">{{author}}<br></span>{{text}} </div>' +
         '<div class="clear"></div>' +
         '</div>',
 });
@@ -9,10 +19,24 @@ Vue.component('message', {
 new Vue({
     el: "#main",
     data: {
+        currentID: 2,
         messages: [
-            {id: 1, sender: 'them', text: 'Го на Зюраткуль?'},
-            {id: 2, sender: 'me', text: 'Не, ну а хуль?'},
+            {id: 1, sender: 'them', text: 'Го на Зюраткуль?', author: 'Артем'},
+            {id: 2, sender: 'them', text: 'Не, ну а хуль?', author: 'Артем'},
         ]
+    },
+    methods: {
+        sendMessage: function () {
+            if (this.text !== undefined) {
+                if (this.messages.length >= 8) {
+                    this.messages.shift()
+                }
+                this.currentID++;
+                this.messages.push({id: this.currentID, sender: 'me', text: this.text, author: false});
+                this.text = undefined;
+
+            }
+        }
     }
 });
 
