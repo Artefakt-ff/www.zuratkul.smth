660f668343be8e752895eb94844db728f102046f
Artefakt-ff <fasqwerty123@mail.ru>
2018-11-19 01:54:28 +0300

    написал меню и плавный переход, использовал еще одну библиотеку при hover'ах, почему-то не получилось сделать меню на vuejs, но думаю это не критично

--- js/messages.js
+++ js/messages.js
@@ -8,6 +8,44 @@ $(function () {  // catch enter press
     });
 });
 
+function isAgreement(answer) {
+    let text = answer.toLowerCase(),
+        sample_answers = ['го', 'давай', 'go', 'гоу', 'погнали', 'давай', 'да', 'yes', 'согласен', 'согласна', 'хули', 'готов', 'готова'];
+    for (let i = 0; i < sample_answers.length; i++) {
+        if (text === sample_answers[i]) return true
+    }
+    return false
+
+}
+
+function sound(path) {
+    $('body').append('<audio id="temporary_sound" src="' + path + '" autoplay="autoplay"></audio>');
+    setTimeout(function () {
+        $('#temporary_sound').remove()
+    }, 2000);
+}
+
+function makeMessage(sender, text, author) {
+    main.currentID++;
+    return {id: main.currentID, sender: sender, text: text, author: author}
+}
+
+function controlNumberOfMessages() {
+    const max_number = 7;
+    if (main.messages.length > max_number) {
+        main.messages.shift();
+    }
+}
+
+function sendMessage(sender, text, author) {
+    let message = makeMessage(sender, text, author);
+    if (sender === 'them' && author !== '...') {
+        sound('sounds/message.mp3');
+    }
+    controlNumberOfMessages();
+    main.messages.push(message);
+}
+
 Vue.component('message', {
     props: ['text', 'sender', 'author'],
     template: '<div class="message">' +
@@ -16,7 +54,7 @@ Vue.component('message', {
         '</div>',
 });
 
-new Vue({
+let main = new Vue({
     el: "#main",
     data: {
         currentID: 2,
@@ -27,15 +65,53 @@ new Vue({
     },
     methods: {
         sendMessage: function () {
+            let temp,
+                main_section = $('#main'),
+                another_section = $('#another');
+
             if (this.text !== undefined) {
-                if (this.messages.length >= 8) {
-                    this.messages.shift()
-                }
-                this.currentID++;
-                this.messages.push({id: this.currentID, sender: 'me', text: this.text, author: false});
+                sendMessage('me', main.text, false);
+
+                temp = this.text;
                 this.text = undefined;
 
+                if (isAgreement(temp)) {
+                    setTimeout(function () {
+                        sendMessage('them', '', '...');
+                        setTimeout(function () {
+                            main.messages.pop();
+                            sendMessage('them', 'Отлично', 'Артем');
+                            setTimeout(function () {
+                                sendMessage('them', '', '...');
+                                setTimeout(function () {
+                                    main.messages.pop();
+                                    sendMessage('them', 'Сейчас скину инфу', 'Артем');
+                                    setTimeout(function () {
+                                        main_section.fadeOut(500);
+                                        another_section.css('display', 'block');
+                                        setTimeout(function () {
+                                            main_section.css('display', 'none');
+                                        }, 500);
+                                    }, 800);
+                                }, 400)
+                            }, 300);
+                        }, 200);
+                    }, 600);
+
+                }
+                else {
+
+                    setTimeout(function () {
+                        sendMessage('them', '', '...');
+                        setTimeout(function () {
+                            main.messages.pop();
+                            sendMessage('them', 'Надеюсь я тебя неправильно понял', 'Артем');
+                        }, 800);
+                    }, 500);
+                }
+
             }
+
         }
     }
 });
